644f79f030306557add931b62d210d0d39b3d22a
Максим Бруцкий <turbosniker@yandex.ru>
2022-03-23 22:33:42 +0300

    Правки после ревью

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,7 +5,7 @@ from .models import Group
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()
     context = {
         'posts': posts,
     }
@@ -15,7 +15,7 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all()
     context = {
         'group': group,
         'posts': posts,
