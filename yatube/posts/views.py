5a3f43482e65d2b808b508c36d685d02b0021da7
Максим Бруцкий <turbosniker@yandex.ru>
2022-03-26 20:24:11 +0300

    third revue

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .models import Group
 
+posts_count = 10
+
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.all()
+    posts = Post.objects.all()[:posts_count]
     context = {
         'posts': posts,
     }
@@ -15,7 +17,7 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()
+    posts = group.posts.all()[:posts_count]
     context = {
         'group': group,
         'posts': posts,
