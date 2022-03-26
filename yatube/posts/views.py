71ccfb20f39a3b80f74ff6887e809c91d484008f
Максим Бруцкий <turbosniker@yandex.ru>
2022-03-26 21:27:00 +0300

    forth revue v.2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -7,7 +7,7 @@ POSTS_COUNT = 10
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.all()[:POSTS_COUN]
+    posts = Post.objects.all()[:POSTS_COUNT]
     context = {
         'posts': posts,
     }
@@ -17,7 +17,7 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:POSTS_COUN]
+    posts = group.posts.all()[:POSTS_COUNT]
     context = {
         'group': group,
         'posts': posts,
