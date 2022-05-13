e198793e1a6651a88706ad73f658175d49fa6ca0
Максим Бруцкий <turbosniker@yandex.ru>
2022-05-13 22:28:01 +0300

    закину на гит на всякий случай. многое недоделано

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'about.apps.AboutConfig',
+    'core.apps.CoreConfig',
+    'users.apps.UsersConfig',
     'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -64,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.year.year',
             ],
         },
     },
@@ -122,3 +126,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+LOGIN_URL = 'users:login'
+LOGOUT_URL = 'users:logout'
+LOGIN_REDIRECT_URL = 'posts:index'
+#LOGOUT_REDIRECT_URL = 'posts:index'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -19,4 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls', namespace='users')),
+    path('about/', include('about.urls', namespace='about')),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
